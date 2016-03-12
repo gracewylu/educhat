79c9eed1ca308316406a614ed398f5036fd679b6
Michael Rhodes <michaelleerhodes@gmail.com>
2016-03-12 17:49:39 -0600

    Made the department POST variable reflect the dropdown

--- public/js/chat.js
+++ public/js/chat.js
@@ -48,7 +48,7 @@ function init(){
 
    function addRoom(){
       var room_name = $('#class').val();
-      var department = $('#dept').val();
+      var department = $('#departments option:selected').val();
       var adminstrator = $('#admin').val();
       var pass = $('#password').val();
       $.ajax({
@@ -101,6 +101,7 @@ function init(){
 var room_name = getParameterByName('room_name');
 document.getElementById('class_name').innerHTML = '<h1>' + room_name + '</h1>';
 $(document).ready(function(){
+    $("select").material_select();
    $('.modal-trigger').leanModal(); //allows modals to show
    init();
 });
